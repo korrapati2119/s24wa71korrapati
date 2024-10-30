36d088d3536394ab756677fb7632e85c849e1d36
venkata kishore korrapati <s575100@nwmissouri.edu>
2024-10-30 00:10:45 -0500

    value calculated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let value = 1;
+let total = 11;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  value += 2;
+  total += value;
+  res.send('The total value is: ${total}');
 });
 
 module.exports = router;
